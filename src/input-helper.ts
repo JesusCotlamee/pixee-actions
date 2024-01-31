afbfafb0b05b5b91a811aa3497817332573f4553
JesusCotlame <jcotlame@iwa.com.mx>
2024-01-31 15:00:02 -0600

    update required input

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -8,8 +8,8 @@ export type Inputs = 'file' | 'url' | 'tool'
  * Helper to get all the inputs for the action
  */
 export function getInputs(): UploadInputs {
-    const file = getRequiredInput('file');
     const url = core.getInput('url');
+    const file = getRequiredInput('file');
     const tool = getRequiredInput('tool');
 
     return {file, url, tool} as UploadInputs
